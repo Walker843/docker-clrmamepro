d90ac8c415512cfda4cb7c20a16ddc8df04e75ca
Walker843 <95179362+Walker843@users.noreply.github.com>
2026-04-11 13:28:04 -0500

    Update startapp.sh
    Moving folder permissions to DockerFIle.

--- startapp.sh
+++ startapp.sh
@@ -2,14 +2,6 @@
 export WINEPREFIX=/config/wine
 export WINEDEBUG=-all
 
-# Fix ownership of CLRMamePro app files only
-chown app:app /opt/clrmamepro/*.exe
-chown app:app /opt/clrmamepro/*.dll
-chown app:app /opt/clrmamepro/*.ini
-chown app:app /opt/clrmamepro/*.cfg
-chown app:app /opt/clrmamepro/*.xml
-chown app:app /opt/clrmamepro/*.txt
-
 # Initialize Wine prefix on first run
 if [ ! -d "$WINEPREFIX" ]; then
     echo "Initializing Wine prefix..."
